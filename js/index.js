cff784176977d1ea1f45b4405979dac6f41cd2e9
denischagin <denischaginnn@gmail.com>
2023-02-05 01:02:20 +0300

    100

--- js/index.js
+++ js/index.js
@@ -115,12 +115,12 @@ blogCardDiv.addEventListener(
 );
 
 function handleGesture() {
-  if (touchstartX - touchendX > 130) {
+  if (touchstartX - touchendX > 100) {
     // left swipe
     flippingCards((indexBlog + 1) % 3);
   }
 
-  if (touchendX - touchstartX > 130) {
+  if (touchendX - touchstartX > 100) {
     // right swipe
     flippingCards((indexBlog + 2) % 3);
   }
